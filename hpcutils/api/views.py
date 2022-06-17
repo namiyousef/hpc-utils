d9cfff2424a12b9c8cf393d6fdd145a6e85d8e4b
Yousef <namiyousef@hotmail.com>
2022-06-17 23:51:05 +0100

    fixes views.py

--- hpcutils/api/views.py
+++ hpcutils/api/views.py
@@ -1,6 +1,7 @@
-from hpcutils.config import SCRIPTS_PATH, TEMPLATES_PATH, CLUSTER_RESOURCE_MAPPING
+from hpcutils.config import SCRIPTS_PATH, TEMPLATES_PATH, CLUSTER_RESOURCE_MAPPING, HELPERS_PATH
 import os
 import json
+import subprocess
 
 def health_check():
     return 'OK'
@@ -13,7 +14,7 @@ def create_cluster_config(cluster, project_name, github_username, github_reposit
 
     job_metadata = os.path.join(cluster_storage_dir, 'job_metadata')
     if not os.path.exists(job_metadata):
-        os.mkdir('job_metadata')
+        os.mkdir(job_metadata)
 
     project_path = os.path.join(job_metadata, project_name)
     if os.path.exists(project_path):
@@ -28,131 +29,84 @@ def create_cluster_config(cluster, project_name, github_username, github_reposit
             ), f)
         return f"Project {project_name} created.", 200
 
-def run_gpu_job(cluster, project_name, job_name, walltime, storage_mem, script_template_name, env_vars):
+def run_gpu_job(body, cluster, project_name, job_name, script_template_name, env_vars):
     cluster_storage_dir = CLUSTER_RESOURCE_MAPPING[cluster]['cluster_storage_dir']
-    project_path = os.path.join(cluster_storage_dir, project_name)
+    project_path = os.path.join(cluster_storage_dir, 'job_metadata', project_name)
     if not os.path.exists(project_path):
         return f"Can't find project {project_name}. Create it using the /create endpoint first", 400
 
     job_path = os.path.join(project_path, job_name)
     if os.path.exists(job_path):
         return f"Job {job_name} already exists", 400
-    else:
-        os.mkdir(job_path)
+
+    os.mkdir(job_path)
+    job_output_path = os.path.join(job_path, 'job_output')
+    os.mkdir(job_output_path)
+
+    metadata_path = os.path.join(project_path, 'metadata.json')
+    try:
+        with open(metadata_path, 'r') as f:
+            metadata = json.load(f)
+
+        github_username = metadata['github_username']
+        github_repository = metadata['github_repository']
+    except Exception as e:
+        return f'Failed to retrieve GitHub username and password. Metadata.json not configured properly or does not exist', 400
 
     venv_path = os.path.join(job_path, 'venv')
+    try:
+        helper_path = os.path.join(HELPERS_PATH, f'{cluster}.sh')
+        output = subprocess.check_output([f'source {helper_path};', f'prepare_virtualenv {venv_path} {github_username} {github_repository}'], shell=True)
+    except subprocess.CalledProcessError as e:
+        return f"Failed to source {helper_path}. Full logs: {e.output}", 400
 
-    # TODO create venv
-
-    # TODO save job metadata
-
-    myriad_template_path = os.path.join(TEMPLATES_PATH, 'myriad.sh')
+    myriad_template_path = os.path.join(TEMPLATES_PATH, f'{cluster}.sh')
     job_script_template_path = os.path.join(SCRIPTS_PATH, script_template_name)
 
-    with open(myriad_template_path, 'r') as f:
-        myriad_script = f.read()
-        myriad_script = myriad_script.format(
-            walltime=walltime,
-            storage_mem=storage_mem,
-            script_template_name=script_template_name,
-            job_name=job_name
-        )
-
-    with open(f'Scratch/job_scripts/job_script.sh', 'w') as f:
-        f.write(myriad_script)
-
-    # build job script
-    with open(job_script_template_path, 'r') as f:
-        job_script = f.read()
-        job_script = job_script.format(
-            **env_vars
-        )
-
-        job_script_function = f'run_job_script () {{' \
-                              f'\n\t{job_script}\n' \
-                              f'}}'
-
-    with open(f'Scratch/{script_template_name}', 'w') as f:
-        f.write(job_script_function)
-
-    # run job # TODO do this properly
-    os.system(
-        f'qsub Scratch/job_scripts/job_script.sh'
-    )
-    return 'f', 200
+    try:
+        with open(myriad_template_path, 'r') as f:
+            myriad_script = f.read()
+            myriad_script = myriad_script.format(
+                project_name=project_name,
+                job_name=job_name,
+                script_template_name=script_template_name,
+                **body
+            )
+    except Exception as e:
+        return f"Failed to read {myriad_template_path}. Full logs: {e}", 400
 
 
-def gpu_myriad(project_name, walltime, storage_mem, script_template_name, env_vars, job_name):
-
-    cluster_storage_dir = CLUSTER_RESOURCE_MAPPING[cluster]['cluster_storage_dir']
-
-    myriad_template_path = os.path.join(TEMPLATES_PATH, 'myriad.sh')
-    job_script_template_path = os.path.join(SCRIPTS_PATH, script_template_name)
-
-    with open(myriad_template_path, 'r') as f:
-        myriad_script = f.read()
-        myriad_script = myriad_script.format(
-            walltime=walltime,
-            storage_mem=storage_mem,
-            script_template_name=script_template_name,
-            job_name=job_name
-        )
-
-    with open(f'Scratch/job_scripts/job_script.sh', 'w') as f:
-        f.write(myriad_script)
+    job_script_path = os.path.join(job_path, 'job_script.sh')
+    try:
+        with open(job_script_path, 'w') as f:
+            f.write(myriad_script)
+    except Exception as e:
+        return f'Failed to write {job_script_path}. Full logs: {e}', 400
 
     # build job script
-    with open(job_script_template_path, 'r') as f:
-        job_script = f.read()
-        job_script = job_script.format(
-            **env_vars
-        )
+    try:
+        with open(job_script_template_path, 'r') as f:
+            job_script = f.read()
+            job_script = job_script.format(
+                **env_vars
+            )
 
-        job_script_function = f'run_job_script () {{' \
-                              f'\n\t{job_script}\n' \
-                              f'}}'
+            job_script_function = f'run_job_script () {{' \
+                                  f'\n\t{job_script}\n' \
+                                  f'}}'
+    except Exception as e:
+        return f"Failed to read {job_script_template_path}. Full logs: {e}", 400
 
-    with open(f'Scratch/{script_template_name}', 'w') as f:
-        f.write(job_script_function)
-
-    # run job # TODO do this properly
-    os.system(
-        f'qsub Scratch/job_scripts/job_script.sh'
-    )
-    return 'f', 200
+    job_script_function_path = os.path.join(job_path, script_template_name)
+    try:
+        with open(job_script_function_path, 'w') as f:
+            f.write(job_script_function)
+    except Exception as e:
+        return f"Failed to write {job_script_function_path}. Full logs: {e}", 400
 
 
-if __name__ == '__main__':
-    walltime='walltime'
-    storage_mem='storage_mem'
-    script_template_name='div_examples.sh'
-    job_name='job_name'
-    env_vars = {
-        'MODEL_NAME': 'django', 'TRAINING_METHOD': 'TRAINING_METHOD', 'LANGS': 'LANGS', 'MAX_LENGTH': 'MAX_LENGTH', 'DATASET': 'DATASET', 'BATCH_SIZE':'BATCH_SIZE', 'EPOCHS': 'EPOCHS', 'VERBOSE': 'VERBOSE', 'SEED': 'SEED'}
-
-    # build myriad script
-    with open('../../templates/myriad.sh', 'r') as f:
-        myriad_script = f.read()
-        myriad_script = myriad_script.format(
-            walltime=walltime,
-            storage_mem=storage_mem,
-            script_template_name=script_template_name,
-            job_name=job_name
-        )
-        print(myriad_script)
-
-    # build job script
-    with open(f'../../scripts/{script_template_name}', 'r') as f:
-        job_script = f.read()
-        job_script = job_script.format(
-            **env_vars
-        )
-
-        job_script_function = f'run_job_script () {{' \
-                              f'\n\t{job_script}\n' \
-                              f'}}'
-
-    with open(f'Scratch/{script_template_name}', 'w') as f:
-        f.write(job_script_function)
-
-    # cleanup (delete temp job script)
+    try:
+        output = subprocess.check_output([f'qsub {job_script_path}'])
+    except Exception as e:
+        return f"Failed to submit job. Full logs: {e}", 400
+    return 'Successfully submitted job', 200
