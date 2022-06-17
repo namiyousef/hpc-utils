24081918228669e8287f1dbc02e0a8823b3730bc
Yousef <namiyousef@hotmail.com>
2022-06-17 23:50:22 +0100

    adds resource mapping to config.py

--- hpcutils/config.py
+++ hpcutils/config.py
@@ -4,3 +4,18 @@ import hpcutils
 DIR_PATH = os.path.dirname(hpcutils.__file__)
 TEMPLATES_PATH = os.path.join(DIR_PATH, 'templates')
 SCRIPTS_PATH = os.path.join(DIR_PATH, 'scripts')
+HELPERS_PATH = os.path.join(DIR_PATH, 'helpers')
+
+
+CLUSTER_RESOURCE_MAPPING = {
+    'myriad': {
+        'cluster_storage_dir': 'Scratch',
+        'venv': {
+            'gpu': {
+                'torch': {
+
+                }
+            }
+        },
+    }
+}
