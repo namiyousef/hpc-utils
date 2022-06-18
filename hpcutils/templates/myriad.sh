1e2c8a7196488701341741ec572bb038f03da3e0
Yousef <namiyousef@hotmail.com>
2022-06-18 22:00:39 +0100

    fixes job_path bug

--- hpcutils/templates/myriad.sh
+++ hpcutils/templates/myriad.sh
@@ -24,7 +24,7 @@
 #$ -wd /home/ucabyn0/Scratch
 
 export PROJECT_PATH=job_metadata/{project_name}
-export JOB_PATH=$PROJECT_PATH/tmp_job_dir
+export JOB_PATH=$PROJECT_PATH/$JOB_ID
 # COPY NECESSARY FILES
 cp -r $JOB_PATH/{script_template_name} $TMPDIR/{script_template_name}
 cp -r $PROJECT_PATH/venv $TMPDIR/venv
